7d4f7655d98271addac41e2e0551b06a7e954362
greenBob24 <92756015+greenBob24@users.noreply.github.com>
2021-10-19 14:01:53 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace sumOfPrimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // сумма простых чисел до 20000000
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            long sum = 5;
+            bool err ;
+            for (int i = 5; i <= 2000000; i=i+2)
+            {
+                err = false;
+                for (int m = 3; m < i; m=m+2)
+                {
+                    if (i % m == 0)
+                    {
+                        err = true;
+                        break;
+                    }
+                }
+                if (err == false)
+                    sum = sum + i;
+
+            }
+            Console.WriteLine(sum);
+            stopWatch.Stop();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+        }
+    }
+}
